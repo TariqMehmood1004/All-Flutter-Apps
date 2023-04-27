d5cdb15e449ec96d33b6f4bf61a15391446bd513
TariqMehmood1004 <116074810+TariqMehmood1004@users.noreply.github.com>
2023-04-27 09:49:50 +0500

    committed

--- 5. Table Generator App/lib/Utilities/utils/constants.dart
+++ 5. Table Generator App/lib/Utilities/utils/constants.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:table_generator_app/Areas/Views/views.dart';
 
-setTimer({int seconds = 0}) => Timer(Duration(seconds: seconds), () {
-      MaterialPageRoute(
-        builder: (_) => MyViewsRoutes.dashboardController,
-      );
-    });
+setTimer({int seconds = 0}) => Timer(
+      Duration(seconds: seconds),
+      () {
+        MaterialPageRoute(
+          builder: (_) => MyViewsRoutes.dashboardController,
+        );
+      },
+    );
