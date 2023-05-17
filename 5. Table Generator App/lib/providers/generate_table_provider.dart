add8803136469ab93085478dc91b6c9bd390dcf5
TariqMehmood1004 <116074810+TariqMehmood1004@users.noreply.github.com>
2023-05-17 23:20:34 +0500

    Using the State Management concepts

---
+++ 5. Table Generator App/lib/providers/generate_table_provider.dart
@@ -0,0 +1,10 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+
+class GenerateTableProvider with ChangeNotifier {
+  late final int tableNumber;
+  late final int startingNumber;
+  late final int endingNumber;
+}
